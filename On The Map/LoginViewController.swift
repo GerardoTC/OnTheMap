7418c520bacbceca2ebec64ecb289caf97da0c80
Gerardo Tarazona Caceres <gtarazonagb@sofi.org>
2020-05-09 18:06:26 -0500

    First commint

---
+++ On The Map/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Gerardo Tarazona Caceres on 5/9/20.
+//  Copyright © 2020 gerardo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
